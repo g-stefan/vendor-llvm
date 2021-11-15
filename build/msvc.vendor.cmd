780b7dba72db6367d948507b8af534b106fcb1c0
Grigore Stefan <g_stefan@yahoo.com>
2021-11-15 23:11:34 +0200

    Update

--- build/msvc.vendor.cmd
+++ build/msvc.vendor.cmd
@@ -24,11 +24,12 @@ rem Source
 pushd archive
 set VENDOR=%PROJECT%-%VERSION%
 if exist %VENDOR%.7z popd && goto:eof
-set WEB_LINK=https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/llvm-project-11.0.0.tar.xz
-if not exist llvm-project-11.0.0.tar.xz curl --insecure --location %WEB_LINK% --output llvm-project-11.0.0.tar.xz
-7z x llvm-project-11.0.0.tar.xz -so | 7z x -aoa -si -ttar -o.
-del /F /Q llvm-project-11.0.0.tar.xz
-move /Y llvm-project-11.0.0 %VENDOR%
+set WEB_LINK=https://github.com/llvm/llvm-project/releases/download/llvmorg-13.0.0/llvm-project-13.0.0.src.tar.xz
+if not exist llvm-project-13.0.0.src.tar.xz curl --insecure --location %WEB_LINK% --output llvm-project-13.0.0.src.tar.xz
+7z x llvm-project-13.0.0.src.tar.xz -so | 7z x -aoa -si -ttar -o.
+del /F /Q llvm-project-13.0.0.src.tar.xz
+move /Y llvm-project-13.0.0.src %VENDOR%
 7zr a -mx9 -mmt4 -r- -sse -w. -y -t7z %VENDOR%.7z %VENDOR%
 rmdir /Q /S %VENDOR%
+del /F /Q pax_global_header
 popd
